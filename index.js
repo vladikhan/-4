4eb6878dd56ef20c6126deb7e459cda2149dcd2e
vladikhan <84037014+vladikhan@users.noreply.github.com>
2022-07-07 04:49:59 +0900

    1

---
+++ index.js
@@ -0,0 +1,16 @@
+let maxCarSpeed = "360km";
+    console.log("У машины изменилась скорость",maxCarSpeed);
+    
+let carOwner = "Mikky";
+    console.log("У машины изменился владелец",carOwner);
+    
+maxCarSpeed = "310km";
+carOwner = "Tony";
+    console.log("У машины изменилась скорость",maxCarSpeed);
+    console.log("У машины изменился владелец",carOwner);
+
+carOwner = "Ferry";
+maxCarSpeed = "330km";
+    console.log("У машины изменилась скорость",maxCarSpeed);
+    console.log("У машины изменился владелец",carOwner);
+
